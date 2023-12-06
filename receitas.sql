feb1114e2c39c26cf48d992a3e8a6d0d69bd3d42
Brenda avelino <117834902+Brenda219@users.noreply.github.com>
2023-12-06 00:55:11 -0300

    Update receitas.sql

--- receitas.sql
+++ receitas.sql
@@ -4,33 +4,38 @@ use Receitas_Caseiras;
 create table receitas
 (
 id_receitas bigint not null auto_increment ,
-nome_receitas varchar(100),
+nome_receitas varchar(255),
 lista_ingredientes varchar (100),
 introducao_preparo varchar(100),
+id_categorias bigint ,
+id_usuarios bigint ,
 primary key(id_receitas)
 );
 create table usuarios
 (
 id_usuarios bigint not null auto_increment,
-endereço_email varchar (20),
-senha varchar(10),
+email varchar (255),
+senha varchar(255),
 primary key(id_usuarios)
 ); 
-create table categoria
+create table categorias
 (
-id_categoria bigint not null auto_increment,
-nome_categoria varchar (100),
-descricao_categoria varchar (100),
-imagem_representativa varchar (100),
-primary key(id_categoria)
+id_categorias bigint not null auto_increment,
+nome_categorias varchar (100),
+descricao_categorias varchar (100),
+primary key(id_categorias)
 );
 create table imagem
 (
 id_imagem bigint not null auto_increment,
 descricao_imagem varchar (100),
+id_receitas bigint,
 primary key(id_imagem)
 );
-
+alter table receitas
+ add foreign key (id_usuarios)references usuarios(id_usuarios),
+add foreign key (id_categorias)references categorias(id_categorias);
+ alter table imagem
+ add  foreign key (id_receitas)references receitas(id_receitas);
  
-
 
